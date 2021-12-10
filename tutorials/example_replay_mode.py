01d4712535017aba721b91a029476727db1c8aa7
hoaquin25 <ge35wir@mytum.de>
2021-12-10 12:14:39 +0100

    change position of example_replay_mode

--- tutorials/example_replay_mode.py
+++ tutorials/example_replay_mode.py
@@ -8,7 +8,7 @@ name = "DEU_Test-1_1_T-1"
 startTime = time.time()
 replay = CarlaReplayMode(scenario_path + name + ".xml", map_path + name + ".xodr")
 
-replay.set_ego_vehicle(replay.find_car_with_CR_ID(6))
+replay.set_ego_vehicle(replay.obstacle_by_id(6))
 # replay.set_ego_vehicle(replay.find_car_with_CR_ID(7))
 # replay.set_ego_vehicle(replay.create_static_obstacles_ego([10, 1.5], 0.0))
 replay.visualize(sleep_time=5, saving_video=False, video_path="/home/")
