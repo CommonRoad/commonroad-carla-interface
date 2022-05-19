bd1a4a57ebb14645a83c176e3137743214a65213
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-05-19 17:12:52 +0200

    bug-fix cr_sim_example_no_mpl.py wrong file endings

--- tutorials/cr_sim_example_no_mpl.py
+++ tutorials/cr_sim_example_no_mpl.py
@@ -19,7 +19,8 @@ time.sleep(5)  # time to move your view in carla-window
 ci.setup_carla(hybrid_physics_mode=False)
 
 startTime = time.time()
-ci.run_scenario(clean_up=True, carla_vehicles=0, carla_pedestrians=0, create_gif=False, gif_path="/home/hoaquin/Desktop/test_image",gif_name="test")
+ci.run_scenario(clean_up=True, carla_vehicles=0, carla_pedestrians=0, create_gif=False,
+                gif_path="/home/hoaquin/Desktop/test_image",gif_name="test")
 executionTime = (time.time() - startTime)
 
 print('Execution time in seconds: ' + str(executionTime))
