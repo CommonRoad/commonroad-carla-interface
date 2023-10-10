370308cec949f7a1b9e93bb2e6fa4a2c9a8c1636
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2023-10-10 13:22:27 +0200

    update reactive planner interface

--- carlacr/controller/reactive_planner.py
+++ carlacr/controller/reactive_planner.py
@@ -1,14 +1,22 @@
+import logging
+
 import numpy as np
 from typing import Optional
 
+from commonroad.common.writer.file_writer_interface import OverwriteExistingFile
 from commonroad.scenario.scenario import Scenario
 from commonroad.scenario.trajectory import Trajectory
-from commonroad.planning.planning_problem import PlanningProblem
+from commonroad.common.file_writer import CommonRoadFileWriter
+from commonroad.planning.planning_problem import PlanningProblem, PlanningProblemSet
 from commonroad_route_planner.route_planner import RoutePlanner
 from commonroad_rp.reactive_planner import ReactivePlanner
 from commonroad_rp.utility.config import ReactivePlannerConfiguration
+from commonroad_rp.state import ReactivePlannerState
 
 from carlacr.helper.planner import TrajectoryPlannerInterface
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class ReactivePlannerInterface(TrajectoryPlannerInterface):
@@ -41,5 +49,18 @@ class ReactivePlannerInterface(TrajectoryPlannerInterface):
         """
         self._config.scenario = sc
         self._config.planning_problem = pp
-        self._planner.reset(self._config)
-        return self._planner.plan()[0]
+        self._planner.reset(self._config,
+                            ReactivePlannerState(position=pp.initial_state.position, velocity=pp.initial_state.velocity,
+                                                 orientation=pp.initial_state.orientation,
+                                                 acceleration=pp.initial_state.acceleration,
+                                                 yaw_rate=pp.initial_state.yaw_rate,
+                                                 time_step=pp.initial_state.time_step, steering_angle=0))
+        try:
+            return self._planner.plan()[0]
+        except AssertionError:
+            logger.error("ReactivePlannerInterface::plan AssertionError: Scenario and "
+                         "Planning Problem will be stored.")
+            fw = CommonRoadFileWriter(sc, PlanningProblemSet([pp]))
+            sc.scenario_id.map_name += "ReactivePlannerError"
+            fw.write_to_file(f"{sc.scenario_id}.xml", OverwriteExistingFile.ALWAYS)
+            exit()
