65ab8dfa313eda3c418de803e037ce5e7a617e2e
Valentin Merle <merle@tufast.de>
2025-03-12 11:23:36 +0100

    add enum for traj vis

--- crcarla/helper/config.py
+++ crcarla/helper/config.py
@@ -62,6 +62,13 @@ class ApproximationType(Enum):
     AREA = 2
 
 
+class TrajectoryVisualization(Enum):
+
+    ONLY_OPTIMAL = 0
+    ONLY_FEASABLE = 1
+    ALL = 2
+
+
 def _dict_to_params(dict_params: Dict, cls: Any) -> Any:
     """
     Converts dictionary to parameter class.
@@ -324,7 +331,7 @@ class WeatherParams(BaseParam):
 class ViewParams(BaseParam):
     """General parameters of CARLA world views."""
 
-    is_visible: bool = True
+    is_visible: bool = True     # turn visualization tools on/off (e.g. trajectories, bounding boxes etc.)
     vis_hud: bool = True
     width: int = 1280
     height: int = 720
@@ -334,8 +341,7 @@ class ViewParams(BaseParam):
     third_person_z_axis_m: float = 4.0
     third_person_angle_deg: float = -20.0
 
-    traj_show_only_optimal: bool = True
-    traj_show_only_feasible: bool = True
+    trajectory_vis: TrajectoryVisualization = TrajectoryVisualization.ONLY_OPTIMAL
 
     @property
     def camera_transform_bird_values(self) -> carla.Transform:
